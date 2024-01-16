d7e374fc272e49828a71c101406bb81e7870afc2
Lex Do <aleksandr.don256@google.com>
2024-01-16 19:31:49 +0200

    MVP

---
+++ Invoice/Diarize.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import torch
+import pickle
+from pydub import AudioSegment
+from pyannote.audio import Pipeline
+
+class Diarizer:
+    def __init__(self):
+        self._diary = list()
+        self._audio_path=''
+        self.device = "cuda" 
+        if not torch.cuda.is_available():
+            device = "cpu"
+        
+        self.pipeline = Pipeline.from_pretrained(
+            "pyannote/speaker-diarization-3.1",
+            use_auth_token="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", )
+        
+        self.pipeline.to(torch.device(self.device))
+                
+    def Diarize(self, audio_path):
+        self._diary = list() 
+        diary_folder = audio_path.split(".wav")[0]
+        diarization = self.pipeline(audio_path)                
+        audio = AudioSegment.from_wav(audio_path)
+        os.mkdir(diary_folder)   
+        
+        for turn, _, speaker in diarization.itertracks(yield_label=True):
+            start = turn.start
+            end = turn.end
+            
+            self._diary.append([start, end, speaker])
+            segment = audio[start:end]
+            
+        with open(diary_folder+'\\diary.pickle', 'wb') as file:
+            pickle.dump(self._diary, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+#outputs separate audio chunks of speach in wav format and csv catalog into audio_path dir (first argument)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing argument audio_path")
+    else:
+        diarizer = Diarizer()
+        diarizer.Diarize(sys.argv[1])
+        
--- Invoice/Transcribe.py
+++ Invoice/Transcribe.py
@@ -40,6 +40,10 @@ class Transcriber:
             torch_dtype=torch_dtype,
             device=device,
         )
+    #Here we are matching transcript data with speaker diarization
+    #Some voice generation models are limited on the size of prompt
+    #So TODO will be more elaborate way of sentense separation
+    #i.e. break down diary record into chunks using timestamps from transcript where each chunk size>200 chars
     def _resolveDiary(self, time, text):
         for rec in self.diary:
             if time>rec[0] and time<rec[1]:
@@ -55,28 +59,56 @@ class Transcriber:
             for chunk in chunks:
                 avg_time = (chunk['timestamp'][1]+chunk['timestamp'][0])/2
                 self._resolveDiary(avg_time,chunk['text'])
-            
+                
+            #stich sentense
+            i=0
+            while i < len(self.diary)-1:
+                text = self.diary[i][3].strip()
+                if text=='':
+                    self.diary.pop(i)
+                    i-=1
+                i+=1
+            i=0
+            while i < len(self.diary)-1:
+                cur_speaker = self.diary[i][2]
+                nxt_speaker = self.diary[i+1][2]
+                text = self.diary[i][3].strip()
+                nxt_text = self.diary[i+1][3].strip()
+                if not(text.endswith('.') or text.endswith('?') or text.endswith('!')) and cur_speaker==nxt_speaker:
+                    text += ' '+nxt_text
+                    self.diary[i][3] = text
+                    self.diary[i][1] = self.diary[i+1][1]
+                    self.diary.pop(i+1)
+                    i-=1
+                i+=1
+                
     def Transcribe(self):
         audio = AudioSegment.from_wav(self.audio_path)
         transcription = self.whisper(self.audio_path)
+        
+        self._FitTranscript(transcription['chunks'])
         for rec in self.diary:
             speaker = AudioSegment.silent(0,audio.frame_rate)
             speaker.export(self.wd+f'\\{rec[2]}.wav', format='wav')
-        
-        self._FitTranscript(transcription['chunks'])
+        i=0    
         for rec in self.diary:
             start = rec[0]
             end = rec[1]
             text = rec[3]
             speaker = rec[2]
-            speaker_path = self.wd+f'\\{speaker}.wav'
-            speaker_aud = AudioSegment.from_file(speaker_path)
             rec[3] = GoogleTranslator(source=self.src_lang, target=self.dst_lang).translate(text)
-            rec.append(speaker_path)
-            segment = audio[int(start*1000):int(end*1000)]
-            speaker_aud+=segment
-            speaker_aud.export(speaker_path, format="wav")
             
+            referense_segment = audio[int(start*1000):int(end*1000)]
+            if end-start<10:#short segments dont give good speaker referance
+                speaker_path = self.wd+f'\\{speaker}.wav'
+                speaker_aud = AudioSegment.from_file(speaker_path)
+                speaker_aud+=referense_segment
+                speaker_aud.export(speaker_path, format="wav")
+                rec.append(speaker_path)
+            else:
+                referense_segment.export(self.wd+f'\\{i}.wav')
+                rec.append(self.wd+f'\\{i}.wav')
+            i+=1            
         with open(self.wd+'\\transcript.pickle', 'wb') as file:
             pickle.dump(self.diary, file, protocol=pickle.HIGHEST_PROTOCOL)
         
@@ -86,5 +118,3 @@ if __name__ == "__main__":
     else:
         transcriber = Transcriber(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
         transcriber.Transcribe()
-
-
