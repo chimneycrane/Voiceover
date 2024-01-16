bc1500d96fa492be50eb99c770dabaa289b23d01
Lex Do <aleksandr.don256@google.com>
2024-01-16 09:17:11 +0200

    Replace Files

--- Invoice/conda_requirements.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-pushd %~dp0
-conda install --n invoice -file requirements.txt -y
--- Invoice/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-pushd %~dp0
-conda activate invoice
-python transcribe.py %2 %3 %4 %5
