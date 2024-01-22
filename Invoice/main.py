49353ee708e8afe2d9c3195f934f85e90e19b091
Lex Do <aleksandr.don256@google.com>
2024-01-22 22:58:42 +0200

    fix argumet bugs

--- Invoice/main.py
+++ Invoice/main.py
@@ -6,7 +6,7 @@ import shutil
 #Arguments ProjectDirectory PathToVideo SouceLanguage DestinationLanguage AccentLanguage VoiceChannelLeft=0 VoiceChannelRight=1
 #Language uses code ex: en, ua, fr, de etc.
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 6:
         print("Missing arguments")
     else:
         #abstraction layer to free vram for each subroutine (some objects like Spleeter stay in vram even after exiting scope or autodisposal)
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         arg2 = sys.argv[1]+'/accompaniment_sep.wav'
         arg3 = sys.argv[1]+'/intermitent_stereo.wav'
         subprocess.run('python '+os.getcwd()+f"/Transcribe.py {arg1} {arg} {sys.argv[3]} {sys.argv[4]}")
-        subprocess.run('python '+os.getcwd()+f"/Synthesize.py {arg1} {sys.argv[4]} {arg3}")
+        subprocess.run('python '+os.getcwd()+f"/Synthesize.py {arg1} {sys.argv[5]} {arg2}")
         subprocess.run('python '+os.getcwd()+f"/RecoverVideo.py {sys.argv[2]} {sys.argv[1]} {left} {right}")
         shutil.rmtree(arg1)
         os.remove(arg)
