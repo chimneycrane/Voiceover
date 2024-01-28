5156802556995ac5de9453844061b9fa722fafe1
Lex Do <aleksandr.don256@google.com>
2024-01-28 05:38:33 +0200

    Adding support for Rscript

--- Invoice/2.win_setup_environment.bat
+++ Invoice/2.win_setup_environment.bat
@@ -4,4 +4,8 @@ conda install -y cudatoolkit -c conda-forge
 conda install -y cudnn -c nvidia
 conda install -y cuda -c nvidia
 conda install -y -c conda-forge ffmpeg libsndfile
-python ../setup.py install
+pip install -r requirements.txt
+Rscript -e "install.packages("pbapply", repos = "https://cran.rstudio.com", quiet = TRUE)"
+Rscript -e "install.packages("tuneR", repos = "https://cran.rstudio.com", quiet = TRUE)"
+Rscript -e "install.packages("seewave", repos = "https://cran.rstudio.com", quiet = TRUE)"
+Rscript -e "install.packages("fftw", repos = "https://r-forge.r-project.org", quiet = TRUE)"
