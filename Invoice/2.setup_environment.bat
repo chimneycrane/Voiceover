a17f4776d03c3061c57ebf40565434e0cce0afcd
Lex Do <aleksandr.don256@google.com>
2024-01-28 03:07:47 +0200

    changes

--- Invoice/2.setup_environment.bat
+++ Invoice/2.setup_environment.bat
@@ -4,4 +4,4 @@ conda install -y cudatoolkit -c conda-forge
 conda install -y cudnn -c nvidia
 conda install -y cuda -c nvidia
 conda install -y -c conda-forge ffmpeg libsndfile
-python ../setup.py install
+pip install -r requirements.txt
--- Invoice/3.run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-pushd %~dp0
-python main.py %1 %2 %3 %4 %5
