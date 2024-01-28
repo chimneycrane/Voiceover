fc0ab7a5de1d47d324b03c7bafcd6484bf554f43
Lex Do <aleksandr.don256@google.com>
2024-01-28 04:52:43 +0200

    rename

--- Invoice/2.win_setup_environment.bat
+++ Invoice/2.win_setup_environment.bat
@@ -4,4 +4,4 @@ conda install -y cudatoolkit -c conda-forge
 conda install -y cudnn -c nvidia
 conda install -y cuda -c nvidia
 conda install -y -c conda-forge ffmpeg libsndfile
-pip install -r requirements.txt
+python ../setup.py install
