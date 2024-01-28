f4e2b01a113bbc3011dac7b9f1fe9b475aa0c761
Lex Do <aleksandr.don256@google.com>
2024-01-28 19:32:42 +0200

    Update main.py

--- Invoice/main.py
+++ Invoice/main.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import shutil
 
-#Arguments ProjectDirectory PathToVideo SouceLanguage DestinationLanguage AccentLanguage VoiceChannelLeft=0 VoiceChannelRight=1
+#Arguments ProjectDirectory VideoName SouceLanguage DestinationLanguage AccentLanguage VoiceChannelLeft=0 VoiceChannelRight=1
 #Language uses code ex: en, ua, fr, de etc.
 def main():
     script_directory = os.path.dirname(__file__)
@@ -12,10 +12,10 @@ def main():
         print("Missing arguments")
     else:
         #abstraction layer to free vram for each subroutine (some objects like Spleeter stay in vram even after exiting scope or autodisposal)
-        
-        subprocess.run(['spleeter','separate','-o', sys.argv[1] ,'-p', 'spleeter:2stems', sys.argv[2]])
-        proj = sys.argv[2].split('.mp4')[0]
-        arg = sys.argv[2].split('.mp4')[0]+'/vocals.wav'
+        video_path = sys.argv[1]+'/'+sys.argv[2]
+        subprocess.run(['spleeter','separate','-o', sys.argv[1] ,'-p', 'spleeter:2stems', video_path])
+        proj = video_path.split('.mp4')[0]
+        arg = video_path.split('.mp4')[0]+'/vocals.wav'
         vocals = proj+'/vocals'
         arg2 = proj+'/accompaniment.wav'
         subprocess.run(['python',script_directory+f"/SplitAudio.py", proj])
@@ -23,7 +23,7 @@ def main():
         subprocess.run(['python',script_directory+f"/Transcribe.py", vocals, arg, sys.argv[3]])
         subprocess.run(['python',script_directory+f"/Translate.py", proj, script_directory, sys.argv[3], sys.argv[4]])        
         subprocess.run(['python',script_directory+f"/synthesize.py", vocals, sys.argv[5], arg2])
-        subprocess.run(['python',script_directory+f"/RecoverVideo.py", sys.argv[2], vocals, sys.argv[1]])
+        subprocess.run(['python',script_directory+f"/RecoverVideo.py", video_path, vocals, sys.argv[1]])
         shutil.rmtree(vocals)
         
 if __name__ == "__main__":
