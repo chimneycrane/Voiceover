a8fcfb6559979ce69cfa73119149e0591c42d93a
Lex Do <aleksandr.don256@google.com>
2024-01-28 07:07:37 +0200

    Update main.py

--- Invoice/main.py
+++ Invoice/main.py
@@ -20,7 +20,7 @@ def main():
         arg2 = proj+'/accompaniment.wav'
         subprocess.run(['python',script_directory+f"/SplitAudio.py", proj])
         subprocess.run(['python',script_directory+f"/Diarize.py",arg])
-        subprocess.run(['python',script_directory+f"/Transcribe.py", vocals, arg])
+        subprocess.run(['python',script_directory+f"/Transcribe.py", vocals, arg, sys.argv[3]])
         subprocess.run(['python',script_directory+f"/Translate.py", proj, script_directory, sys.argv[3], sys.argv[4]])        
         subprocess.run(['python',script_directory+f"/Synthesize.py", vocals, sys.argv[5], arg2])
         subprocess.run(['python',script_directory+f"/RecoverVideo.py", sys.argv[2], vocals, sys.argv[1]])
