aa5586f1a9caf7485e22c2ace66d11517d0620a9
chimneycrane <156237370+chimneycrane@users.noreply.github.com>
2024-02-10 04:39:02 +0300

    Update ExtractFeatures.py

--- Invoice/ExtractFeatures.py
+++ Invoice/ExtractFeatures.py
@@ -31,12 +31,12 @@ def _feature_extraction(sound_file, start, end, selec, bp, wl, threshold):
     median_frequency = librosa.hz_to_mel(np.median(spectral_centroids)) / 1000
     mel_spec = librosa.feature.melspectrogram(y=audio, sr=sr)
     mel_spec_db = librosa.power_to_db(mel_spec)
+    freqs = librosa.mel_frequencies(n_mels=mel_spec.shape[0], fmin=0, fmax=sr/2)
+    freqs_khz = freqs / 1000 
     interp_funcs = [interp1d(mel_spec_db[:, i], freqs_khz[:, i]) for i in range(mel_spec_db.shape[1])]
     q25 = np.percentile(mel_spec_db, 25)
     q75 = np.percentile(mel_spec_db, 75)
     iqr = q75 - q25
-    freqs = librosa.mel_frequencies(n_mels=mel_spec.shape[0], fmin=0, fmax=sr/2)
-    freqs_khz = freqs / 1000
     q25_khz = np.array([interp_func(q25) for interp_func in interp_funcs]).T
     q75_khz = np.array([interp_func(q75) for interp_func in interp_funcs]).T
     iqr_khz = q75_khz - q25_khz
