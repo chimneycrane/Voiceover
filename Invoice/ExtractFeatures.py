46ec998c52f4d49e1774959627fe42e5922ef59d
chimneycrane <156237370+chimneycrane@users.noreply.github.com>
2024-02-14 18:20:25 +0300

    Update ExtractFeatures.py

--- Invoice/ExtractFeatures.py
+++ Invoice/ExtractFeatures.py
@@ -142,8 +142,8 @@ def specan3(X, bp=(0, 22), wl=2048, threshold=5, parallel=1):
     # Combine features into data frame and rename columns
     df = pd.DataFrame(features).transpose()
     print(features[0].keys())   
-    key_list = [key for key in features[0].keys()]
-    df.columns = ["sound.files", "selec"]+key_list
+    key_list = list(features[0].keys())
+    df.columns = ["sound.files", "selec"] + key_list
     print(df.columns)
     df.set_index("selec", inplace=True)
     df.index.names = ["selection"]
