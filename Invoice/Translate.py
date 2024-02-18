ccf3e0a673b4f6b0b7693122259e155a4f031c24
chimneycrane <156237370+chimneycrane@users.noreply.github.com>
2024-02-18 05:21:17 +0300

    Update Translate.py

--- Invoice/Translate.py
+++ Invoice/Translate.py
@@ -5,6 +5,17 @@ from language_tool_python import LanguageTool
 from deep_translator import GoogleTranslator
 from pydub import AudioSegment
 from langdetect import detect
+from num2words import num2words
+
+def replace_numbers_with_words(text):
+    words = text.split()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_words.append(num2words.num2words(int(word)))
+        else:
+            new_words.append(word)
+    return " ".join(new_words)
 
 diary =[]
 with open(sys.argv[1]+'/transcript.pickle', 'rb') as file:
@@ -21,6 +32,7 @@ for rec in diary:
             grammar_modifier[rec[2]]='male'#predict(sys.argv[1]+f"/{rec[2]}.wav", sys.argv[2])
         feature = grammar_modifier[rec[2]]
         translation = GoogleTranslator(source=sys.argv[3], target=sys.argv[4]).translate(f'({feature}): '+rec[3])
+        translation = replace_numbers_with_words(translation)
         rec[3] = tool.correct(translation).split('):')[1]
         rec.append(1)
     else:
