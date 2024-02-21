ae4bc874282a34ff649eb9a6ddb4a089e466c877
chimneycrane <156237370+chimneycrane@users.noreply.github.com>
2024-02-21 21:17:57 +0300

    Update Translate.py

--- Invoice/Translate.py
+++ Invoice/Translate.py
@@ -33,7 +33,7 @@ for rec in diary:
         feature = grammar_modifier[rec[2]]
         rec[3] = replace_numbers_with_words(rec[3])
         translation = GoogleTranslator(source=sys.argv[3], target=sys.argv[4]).translate(f'({feature}): '+rec[3])
-        
+        print(rec)
         if rec[3].find('：')>=0:
             rec[3] = tool.correct(translation).split('：')[1]
         else:
